3eea536af943822b7a7fde38e431add652897e1e
RConradGe <rcgadduang@gmail.com>
2025-03-05 16:02:14 -0800

    Updated queries, env variable, added model

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+/*------------------------------- Requirements/Imports -------------------------------*/
+
 const prompt = require("prompt-sync")();
 
 const username = prompt("What is your name?");
 
 console.log(`Your name is ${username}`);
+
 
